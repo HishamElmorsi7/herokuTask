af6192198ae299140d1d060c63d07f634c5b9489
HishamElmorsi7 <74022699+HishamElmorsi7@users.noreply.github.com>
2023-12-11 14:15:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Task\Controllers\Product;
 // Defining the directory separator that depends in the OS
 define('DS', DIRECTORY_SEPARATOR);
 // requiring config file
-require_once APP_PATH . DS . 'config' . DS . 'config.php';
+require_once 'app' . DS . 'config' . DS . 'config.php';
 // requiring the autoloader
 require_once APP_PATH . DS . 'lib' . DS . 'autoload.php';
 
