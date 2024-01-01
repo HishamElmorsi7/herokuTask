4ce6ca04f019add76964fc4e2450ced13d0c477c
HishamElmorsi7 <74022699+HishamElmorsi7@users.noreply.github.com>
2024-01-01 09:07:47 +0200

    Access config vars of heroku

--- app/lib/databasehandler.php
+++ app/lib/databasehandler.php
@@ -5,10 +5,10 @@ class DatabaseHandler
 {
 
     // Database handler attributes
-    private $host = DB_HOST;
-    private $name = USER;
-    private $pwd = PWD;
-    private $dbname = DB_NAME;
+    private $host = getenv('DB_HOST');
+    private $name = getenv('USER');
+    private $pwd = getenv('PWD');
+    private $dbname = getenv('DB_NAME');
 
     public function connect()
     {
