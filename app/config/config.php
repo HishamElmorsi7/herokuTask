102a799bc7864312808d3573fd041d665a12ce12
HishamElmorsi7 <74022699+HishamElmorsi7@users.noreply.github.com>
2024-01-01 08:34:45 +0200

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -7,11 +7,4 @@ if (!defined('DS')) {
 //paths info
 define('APP_PATH', realpath(dirname(dirname(__FILE__))));
 define('VIEWS_PATH', APP_PATH . DIRECTORY_SEPARATOR . 'views');
-
-
-//Connection info
-define('DB_HOST', 'sql5.freemysqlhosting.net');
-define('USER', 'sql5669517');
-define('PWD', 'w5VHSbbD5T');
-define('DB_NAME', 'sql5669517');
 define('SITE_NAME', 'task');
